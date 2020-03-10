b7372575a8ade8ac8299a97fc09ffd682ce49be2
Cesar Dias <cesarvdias@hotmail.com>
2020-03-10 12:58:38 -0300

    Exercicio1

---
+++ ExibeTresFrasesAoContrario.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+public class ExibeTresFrasesAoContrario {
+   public static void main (String [] args) {
+      //Declarar vari�veis 
+      String palavra1;
+      String palavra2;
+      String palavra3;
+      
+      //Entrada de dados
+      palavra1 = JOptionPane.showInputDialog ("Digite a primeira palavra");
+      palavra2 = JOptionPane.showInputDialog ("Digite a segunda palavra");
+      palavra3 = JOptionPane.showInputDialog ("Digite a terceira palavra");
+      
+      //Processamento
+      String resultado = palavra3 + ' ' + palavra2 + ' ' + palavra1;
+      
+      //Sa�da
+      JOptionPane.showMessageDialog (null, resultado); 
+      
+   }
+
+}
